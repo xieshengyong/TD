eafd7e7caedcd2a8a8fe670225248bdf94c4aa97
xieshengyong <m@snowzen.cn>
2019-07-05 22:57:39 +0800

    合并固定库，加快打包速度

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-/*
-*
-*  引入lib库文件和LESS文件
-*  必须要引入,过滤器会过滤lib文件夹里面的JS文件,做一个简单的复制
-*  复制到相应的文件夹
-*  引入的less会对less进行编译存放到css文件夹
-* */
-import 'zepto/src/zepto';
-import 'zepto/src/event';
-import 'zepto/src/fx';
-import 'zepto/src/fx_methods';
-
-import 'howler';
-import * as PIXI from 'pixi.js'
-
 // 引入的包根据实际情况而定
 import LoadViewController from './app/LoadViewController';
 import IndexViewController from './app/IndexViewController';
@@ -33,11 +18,6 @@ var init = function () {
         loadView.onhide = indexPageBack;
 
         loadView.load();
-
-        const qqq = (params) => {
-            console.log(7);
-        }
-        qqq();
     };
 
     // index页面
---
+++ src/js/library.js
@@ -0,0 +1,18 @@
+/**
+ * zepto
+ */
+import 'zepto/src/zepto';
+import 'zepto/src/event';
+import 'zepto/src/fx';
+import 'zepto/src/fx_methods';
+
+/**
+ * howler 音频库
+ */
+import 'howler';
+
+/**
+ * PIXI
+ */
+import * as PIXI from 'pixi.js';
+window.PIXI = PIXI;
