ad3f1ecfec177bacd75a627b270997f19dbf6f65
xieshengyong <m@snowzen.cn>
2019-07-10 17:53:48 +0800

    style：去除debug代码

--- src/js/app/module/Orient.js
+++ src/js/app/module/Orient.js
@@ -1,33 +1,24 @@
 // 判断横竖屏
 // 参考： https://aotu.io/notes/2017/01/31/detect-orientation/index.html
 
-function detectOrient () {
-    var cw = document.documentElement.clientWidth;
-    var sw = window.screen.width;
-    var sh = window.screen.height;
+(function (doc, win) {
+    function detectOrient () {
+        var cw = document.documentElement.clientWidth;
+        var sw = window.screen.width;
+        var sh = window.screen.height;
 
-    [sw, sh] = sw < sh ? [sw, sh] : [sh, sw]; // Android下出现 srceen.width/height 值交换，所以进行大小值比较判断
+        [sw, sh] = sw < sh ? [sw, sh] : [sh, sw]; // Android下出现 srceen.width/height 值交换，所以进行大小值比较判断
 
-    console.log(screen.width, screen.height);
-    console.log(cw, sw, sh);
-
-    var detectData = document.querySelector('.m-wrap');
-    detectData.innerHTML = 'clientWidth；' + document.documentElement.clientWidth + ';<br>' +
-        'clientHeight；' + document.documentElement.clientHeight + ';<br>' +
-        'sw：' + sw + ';<br>' +
-        'sh: ' + sh + ';<br>' +
-        'screen.width：' + window.screen.width + ';<br>' +
-        'screen.height：' + window.screen.height + ';';
-
-    if (cw === sw) {
-        // 竖屏
-        console.log('竖屏');
-        document.querySelector('html').setAttribute('data-orient', 'portrait');
-    } else {
-        console.log('横屏');
-        document.querySelector('html').setAttribute('data-orient', 'landscape');
+        if (cw === sw) {
+            // 竖屏
+            document.querySelector('html').setAttribute('data-orient', 'portrait');
+        } else {
+            document.querySelector('html').setAttribute('data-orient', 'landscape');
+        };
     };
-}
-
-window.onresize = detectOrient;
-window.onload = detectOrient;
+    var rszEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    win.addEventListener('load', detectOrient, false);
+    win.addEventListener(rszEvt, detectOrient, false);
+    if (!doc.addEventListener) return;
+    doc.addEventListener('DOMContentLoaded', detectOrient, false);
+})(document, window);
