5b62e46af685ea39b101f21158b495853ebf5c19
xieshengyong <m@snowzen.cn>
2019-07-11 23:18:42 +0800

    debug：简化完善布局方案

--- src/js/app/module/Orient.js
+++ src/js/app/module/Orient.js
@@ -1,6 +1,4 @@
 // 判断横竖屏
-// 参考： https://aotu.io/notes/2017/01/31/detect-orientation/index.html
-
 (function (doc, win) {
     function detectOrient () {
         var cw = document.documentElement.clientWidth;
@@ -9,12 +7,7 @@
 
         [sw, sh] = sw < sh ? [sw, sh] : [sh, sw]; // Android下出现 srceen.width/height 值交换，所以进行大小值比较判断
 
-        if (cw === sw) {
-            // 竖屏
-            document.querySelector('html').setAttribute('data-orient', 'portrait');
-        } else {
-            document.querySelector('html').setAttribute('data-orient', 'landscape');
-        };
+        document.querySelector('html').setAttribute('data-orient', cw !== sw ? 'landscape' : 'portrait');
     };
     var rszEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
     win.addEventListener('load', detectOrient, false);
