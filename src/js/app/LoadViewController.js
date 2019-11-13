6e1931d467be482ae4f7e021ef3ef84c2fb3bfb3
xieshengyong <m@snowzen.cn>
2019-11-13 19:43:42 +0800

    clip动画测试

--- src/js/app/LoadViewController.js
+++ src/js/app/LoadViewController.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import TD from './module/TD.js';
+import TD, { pad } from './module/TD.js';
 import {
     Config,
     formatImgList
@@ -35,7 +35,7 @@ export const LoadViewController = {
         this.init();
     },
 
-    clip (data) {
+    clip2Signle (data) {
         let first = data[0]['shape'][0];
         let second = data[0]['shape'][1];
         let end = '';
@@ -43,11 +43,11 @@ export const LoadViewController = {
             let arr = ele1.shape;
             let st = '';
 
-            arr.push(arr[0]);
-            arr.push(arr[1]);
+            // arr.push(arr[0]);
+            // arr.push(arr[1]);
 
-            arr.unshift(second);
-            arr.unshift(first);
+            // arr.unshift(second);
+            // arr.unshift(first);
 
             arr.forEach((ele2, idx) => {
                 let fix = (idx % 2) > 0 ? 'rem, ' : 'rem ';
@@ -59,6 +59,32 @@ export const LoadViewController = {
         let aaa = end.substr(0, end.length - 2);
 
         return aaa;
+    },
+
+    clipSignleGraphics () {
+
+    },
+
+    clip2Mulit (data, wrap) {
+        data.forEach(ele1 => {
+            let arr = ele1.shape;
+            let st = '';
+
+            arr.forEach((ele2, idx) => {
+                let fix = (idx % 2) > 0 ? 'rem, ' : 'rem ';
+                st += ((ele2 / 100) + fix);
+            });
+
+            let end = st.substr(0, st.length - 2);
+            let div = document.createElement('div');
+            div.style.clipPath = 'polygon(' + end + ')';
+            div.style.backgroundColor = getRandomColor();
+            if (wrap instanceof HTMLElement) {
+                wrap.appendChild(div);
+            } else {
+                console.error('wrap 需是dom元素');
+            }
+        });
     },
 
     hollow (data) {
@@ -84,12 +110,43 @@ export const LoadViewController = {
     },
 
     init () {
-        let a = require('./cs2.json');
-        // let aaa = this.clip(a['a']);
+        // let a = require('./cs2.json');
+        // let aaa = this.clip2Signle(a['a']);
         // let aaa = this.hollow(a['a']);
-        let aaa = this.hollow(a['t2']);
-        console.log('end :', aaa);
-        $('.m-loading').css('clip-path', 'polygon(' + aaa + ')');
+        // let aaa = this.hollow(a['t2']);
+        // console.log('end :', aaa);
+        // $('.m-loading').css('clip-path', 'polygon(' + aaa + ')');
+        // this.clip2Mulit(require('./c.json')['c'], $('.m-loading')[0]);
+        // this.clip2Mulit(require('./c.json')['b'], $('.m-loading')[0]);
+
+        for (let index = 0; index < 33; index++) {
+            let div = $('<div class="idx-' + index + '"></div>');
+            div.css('background-color', getRandomColor());
+            $('.m-loading').append(div);
+        }
+
+        const showAnimal = (data, name) => {
+            let divall = $('.m-loading div');
+            let i = 32;
+            while (i > -1) {
+                let ii = i;
+                setTimeout(() => {
+                    let div = divall.eq(ii);
+                    div.css('clip-path', 'polygon(' + this.clip2Signle(data[name + pad(ii, 5)]) + ')');
+                }, i * 30);
+                i--;
+            }
+        };
+
+        let ani2 = require('./ani2.json');
+        let ani3 = require('./ani3.json');
+        showAnimal(ani3, 'ani3_');
+
+        let isNext = !false;
+        $('body').click(() => {
+            isNext ? showAnimal(ani2, 'ani2_') : showAnimal(ani3, 'ani3_');
+            isNext = !isNext;
+        });
     }
 };
 
@@ -141,3 +198,7 @@ export const LoadViewController = {
         script.src = require('../lib/vconsole.min.js');
     }
 }());
+
+var getRandomColor = function () {
+    return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);
+};
